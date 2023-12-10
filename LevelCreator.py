35a35658ae190e4e271735f880c1148918258926
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2023-12-10 13:18:41 -0600

    add some spaces for readability

--- LevelCreator.py
+++ LevelCreator.py
@@ -4,19 +4,24 @@ import socket
 import glob
 import os
 import random as rnd
+
 pg.init()
 
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 800
 AMOGUS = pg.image.load(r'imgz/amogus.png')
+
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 tilePicked = pg.image.load(r'imgz/image1.jpeg')
-mouseX,mouseY = pg.mouse.get_pos()
+mouseX, mouseY = pg.mouse.get_pos()
+
 emouseX = mouseX
 emouseY = mouseY
 scale = 0.5
 yPosImage = False
 xPosImage = False
+
+
 def PickImage():
     global tilePicked
     if(mouseY < 100 and mouseX > 800):
@@ -42,6 +47,8 @@ def PickImage():
         global emouseY
         emouseY = mouseY
         return screen.blit(pg.transform.scale(tilePicked,(100,100)), (emouseX, emouseY))
+    
+
 # --- Create grid of numbers
 # Create an empty list
 # --- Create grid of numbers
@@ -57,14 +64,18 @@ for row in range(10):
         # Add a the number zero to the current row
         grid[row].append(0)
 
+
 while True:
     screen.fill((255,255,255))
     pg.draw.line(screen, (0,0,0), (800,0), (800,800))
+    
     i = 0
     im = 0
+
     while(i < 8/scale+1):
         i+=1
         pg.draw.line(screen, (0,0,0), (i*scale*100,0), (i*scale*100,800))
+
     while(im < 8/scale+1):
         im+=1
         pg.draw.line(screen, (0,0,0), (0,im*scale*100), (800,im*scale*100))
@@ -79,6 +90,7 @@ while True:
     screen.blit(pg.transform.scale(pg.image.load(r'imgz/image7.jpeg'),(100,100)), (800, 600))
     screen.blit(pg.transform.scale(pg.image.load(r'imgz/image8.jpeg'),(100,100)), (800, 700))
     mouseX,mouseY = pg.mouse.get_pos()
+
     if(pg.mouse.get_pressed(3)[0] == True):
        grid[mouseX//100][mouseY//100] = 1
        print(grid)         
@@ -88,6 +100,7 @@ while True:
     for column in range(int(8/scale)):
         for row in range(int(8/scale)):
             screen.blit(pg.transform.scale(pg.image.load(r'imgz/image8.jpeg'),(100*scale,100*scale)), (100*column*scale, 100*row*scale))
+    
     if(grid[mouseX//100][mouseY//100] == 1):
         screen.blit(pg.transform.scale(tilePicked,(100*scale,100*scale)), (100*(200//50)*scale, 100*(299//50)*scale))
         screen.blit(pg.transform.scale(tilePicked,(100*scale,100*scale)), (100*(mouseX//50)*scale, 100*(mouseY//50)*scale))
@@ -99,6 +112,7 @@ while True:
             pos=pg.mouse.get_pos()
             btn=pg.mouse
             print ("x = {}, y = {}".format(pos[0], pos[1]))
+
     pg.display.flip()
     # stuff can go here too
     pg.display.update()
