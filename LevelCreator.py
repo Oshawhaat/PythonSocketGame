063bf7f2d0fea6ebfeb67c8be2fda0642e260e75
Elijah Gray <87554755+JazzyBookworm@users.noreply.github.com>
2023-12-11 14:05:00 -0600

    stick the tiles

--- LevelCreator.py
+++ LevelCreator.py
@@ -9,36 +9,37 @@ pg.init()
 
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 800
+
 AMOGUS = pg.image.load(r'imgz/amogus.png')
 
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 tilePicked = pg.image.load(r'imgz/image1.jpeg')
 
-scale = 0.5
-yPosImage = False
-xPosImage = False
+SCALE = 0.5
+GRID_TO_ARRAY= 100
+MULTIPLE_PREVENTER = 0
 
 
-def pick_image(mouseX, mouseY): #TODO handle if mouse is at exactly a multiple of 100 #TODO so many magic number
+def pick_image(mouseX, mouseY):  #TODO so many magic number
+    global tilePicked
     if not mouseX > 800: return
-
     if(mouseY < 100):
         tilePicked = pg.image.load(r'imgz/image1.jpeg')
-    if(mouseY > 100 and mouseY < 200):
+    if(mouseY > 100 and mouseY <= 200):
         tilePicked = pg.image.load(r'imgz/image2.jpeg')
-    if(mouseY > 200 and mouseY < 300):
+    if(mouseY > 200 and mouseY <= 300):
         tilePicked = pg.image.load(r'imgz/image3.jpeg')
-    if(mouseY > 300 and mouseY < 400):
+    if(mouseY > 300 and mouseY <= 400):
         tilePicked = pg.image.load(r'imgz/image4.jpeg')
-    if(mouseY > 400 and mouseY < 500):
+    if(mouseY > 400 and mouseY <= 500):
         tilePicked = pg.image.load(r'imgz/image5.jpeg')
-    if(mouseY > 500 and mouseY < 600):
+    if(mouseY > 500 and mouseY <= 600):
         tilePicked = pg.image.load(r'imgz/image6.png')
-    if(mouseY > 600 and mouseY < 700):
+    if(mouseY > 600 and mouseY <= 700):
         tilePicked = pg.image.load(r'imgz/image7.jpeg')
-    if(mouseY > 700):
+    if(mouseY >= 700):
         tilePicked = pg.image.load(r'imgz/image8.jpeg')
-    return screen.blit(pg.transform.scale(tilePicked,(100,100)), (mouseX, mouseY))
+    return tilePicked
     
 
 # --- Create grid of numbers
@@ -56,23 +57,24 @@ for row in range(10): #TODO magic number
         # Add a the number zero to the current row
         grid[row].append(0)
 
-
+screen.fill((255,255,255))
 while True:
-    screen.fill((255,255,255))
+    
 
     mouseX, mouseY = pg.mouse.get_pos()
-
+    
     pg.draw.line(screen, (0,0,0), (800,0), (800,800))
 
     i = 0
-    while(i < 8/scale+1): #TODO an iterating while loop is the same as a for loop
+    while(i < 8/SCALE+1): #TODO an iterating while loop is the same as a for loop
         i+=1
-        pg.draw.line(screen, (0,0,0), (i*scale*100,0), (i*scale*100,800)) #TODO magic number
+        pg.draw.line(screen, (0,0,0), (i*SCALE*100,0), (i*SCALE*100,800)) #TODO magic number
 
     im = 0
-    while(im < 8/scale+1): #TODO ^(ln69) #TODO magic number
+    while(im < 8/SCALE+1): #TODO ^(ln69) #TODO magic number
         im+=1
-        pg.draw.line(screen, (0,0,0), (0,im*scale*100), (800,im*scale*100)) #TODO magic number
+        pg.draw.line(screen, (0,0,0), (0,im*SCALE*100), (800,im*SCALE*100)) #TODO magic number
+
 
     #TODO when repeating a bunch of code but slighly differnt, a for loop is almost always the solution #TODO magic number
     screen.blit(pg.transform.scale(pg.image.load(r'imgz/image1.jpeg'),(100,100)), (800, 0))
@@ -83,19 +85,19 @@ while True:
     screen.blit(pg.transform.scale(pg.image.load(r'imgz/image6.png'),(100,100)), (800, 500))
     screen.blit(pg.transform.scale(pg.image.load(r'imgz/image7.jpeg'),(100,100)), (800, 600))
     screen.blit(pg.transform.scale(pg.image.load(r'imgz/image8.jpeg'),(100,100)), (800, 700))
-
+    
     if(pg.mouse.get_pressed(3)[0] == True):
-       grid[mouseX//100][mouseY//100] = 1 #TODO magic number
-       print(grid)         
+        pick_image(mouseX, mouseY)
+        grid[int(mouseX//GRID_TO_ARRAY)][int(mouseY//GRID_TO_ARRAY)] = 1 #TODO magic number
+        MULTIPLE_PREVENTER = 0
+    
+       
 
     
-    for column in range(int(8/scale)): #TODO magic number
-        for row in range(int(8/scale)): #TODO magic number
-            screen.blit(pg.transform.scale(pg.image.load(r'imgz/image8.jpeg'),(100*scale,100*scale)), (100*column*scale, 100*row*scale)) #TODO lots of magic numbers
+
     
-    if(grid[mouseX//100][mouseY//100] == 1): #TODO what is this code for? it is not clear to me when reading it #TODO magic number
-        screen.blit(pg.transform.scale(tilePicked,(100*scale,100*scale)), (100*(200//50)*scale, 100*(299//50)*scale)) #TODO magic number
-        screen.blit(pg.transform.scale(tilePicked,(100*scale,100*scale)), (100*(mouseX//50)*scale, 100*(mouseY//50)*scale)) #TODO magic number
+    if(grid[mouseX//int(GRID_TO_ARRAY)][mouseY//int(GRID_TO_ARRAY)] == 1): #TODO what is this code for? it is not clear to me when reading it #TODO magic number
+        screen.blit(pg.transform.scale(tilePicked,(100*SCALE,100*SCALE)), (100*(mouseX//50)*SCALE, 100*(mouseY//50)*SCALE)) #TODO magic number
     
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -103,6 +105,5 @@ while True:
         if event.type == pg.MOUSEBUTTONDOWN:
             pos=pg.mouse.get_pos()
             btn=pg.mouse
-            print ("x = {}, y = {}".format(pos[0], pos[1])) #TODO you can simply fill the curleys with the var you want by using an f string
 
     pg.display.flip()
