7f94c5db4c6d8f3717ccee6bc19d5b9fc9570f3a
Oshawhat <alannobledall@gmail.com>
2023-12-13 10:19:20 -0600

    simplify if statements in pick_image

--- LevelCreator.py
+++ LevelCreator.py
@@ -11,37 +11,39 @@ TILES_PER_ROW = 10
 TILES_PER_COLUMN = 10
 
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-tilePicked = pg.image.load(r'imgz/image1.jpeg')
+picked_tile = pg.image.load(r'imgz/image1.jpeg')
 
-tile_images = ["imgz/Image1.jpeg",
-               "imgz/Image2.jpeg",
-               "imgz/Image3.jpeg",
-               "imgz/Image4.jpeg",
-               "imgz/Image5.jpeg",
-               "imgz/Image6.png",
-               "imgz/Image7.jpeg",
-               "imgz/Image8.jpeg"]
+tile_image_paths = ["imgz/Image1.jpeg",
+                    "imgz/Image2.jpeg",
+                    "imgz/Image3.jpeg",
+                    "imgz/Image4.jpeg",
+                    "imgz/Image5.jpeg",
+                    "imgz/Image6.png",
+                    "imgz/Image7.jpeg",
+                    "imgz/Image8.jpeg"]
+tile_images = [pg.image.load(image) for image in tile_image_paths]
 
 
 def pick_image(mouseX, mouseY):  # TODO so many magic number
     if mouseX < 800: return
 
+    # TODO this must be compactable
     if mouseY <= 100:
-        tilePicked = pg.image.load(r'imgz/image1.jpeg')
+        tilePicked = tile_images[0]
     if 100 < mouseY <= 200:
-        tilePicked = pg.image.load(r'imgz/image2.jpeg')
+        tilePicked = tile_images[1]
     if 200 < mouseY <= 300:
-        tilePicked = pg.image.load(r'imgz/image3.jpeg')
+        tilePicked = tile_images[2]
     if 300 < mouseY <= 400:
-        tilePicked = pg.image.load(r'imgz/image4.jpeg')
+        tilePicked = tile_images[3]
     if 400 < mouseY <= 500:
-        tilePicked = pg.image.load(r'imgz/image5.jpeg')
+        tilePicked = tile_images[4]
     if 500 < mouseY <= 600:
-        tilePicked = pg.image.load(r'imgz/image6.png')
+        tilePicked = tile_images[5]
     if 600 < mouseY <= 700:
-        tilePicked = pg.image.load(r'imgz/image7.jpeg')
+        tilePicked = tile_images[6]
     if mouseY >= 700:
-        tilePicked = pg.image.load(r'imgz/image8.jpeg')
+        tilePicked = tile_images[7]
     return tilePicked
 
 
@@ -75,19 +77,19 @@ while True:
     im = 0
     while im < 8 / SCALE + 1:  # TODO an iterating while loop is the same as a for loop # TODO magic number
         im += 1
-        pg.draw.line(screen, (0, 0, 0), (0, im * SCALE * 100), (800, im * SCALE * 100))  # TODO magic number
+        pg.draw.line(screen, (0, 0, 0), (0, im * SCALE * 100), (800, im * SCALE * 100))  # TODO magic numbers
 
     for index, image in enumerate(tile_images):
-        screen.blit(pg.transform.scale(pg.image.load(image), (100, 100)), (800, index * 100)) # TODO magic number
+        screen.blit(pg.transform.scale(pg.image.load(image), (100, 100)), (800, index * 100)) # TODO magic numbers
 
     if lmb:
         picked = pick_image(mouseX, mouseY)
         if picked:
-            tilePicked = picked
+            picked_tile = picked
         else:
-            grid[mouseX // GRID_TO_ARRAY][mouseY // GRID_TO_ARRAY] = 1  # TODO magic number
-            screen.blit(pg.transform.scale(tilePicked, (100 * SCALE, 100 * SCALE)),
-                        (100 * (mouseX // 50) * SCALE, 100 * (mouseY // 50) * SCALE))
+            grid[mouseX // GRID_TO_ARRAY][mouseY // GRID_TO_ARRAY] = picked_tile
+            screen.blit(pg.transform.scale(picked_tile, (100 * SCALE, 100 * SCALE)),
+                        (100 * (mouseX // 50) * SCALE, 100 * (mouseY // 50) * SCALE)) # TODO magic numbers
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
