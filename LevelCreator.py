5f57825e5efbb86a6d5e45f0b9c6795d677fb0a3
Oshawhat <alannobledall@gmail.com>
2023-12-15 10:03:24 -0600

    change variables lower case

--- LevelCreator.py
+++ LevelCreator.py
@@ -10,25 +10,27 @@ SIDE_MENU_WIDTH = 100
 TILES_PER_ROW = 15
 TILES_PER_COLUMN = 15
 
-TILE_IMAGE_PATHS = [f"imgz/{image}" for image in os.listdir("imgz") if image.startswith("tile_")]
-TILE_IMAGES = [pg.image.load(image) for image in TILE_IMAGE_PATHS]
+tile_image_paths = [f"imgz/{image}" for image in os.listdir("imgz") if image.startswith("tile_")]
+tile_images = [pg.image.load(image) for image in tile_image_paths]
 
-path_to_image = {image:path for (image, path) in zip(TILE_IMAGE_PATHS, TILE_IMAGES)}
-image_to_path = {path:image for image, path in zip(TILE_IMAGE_PATHS, TILE_IMAGES)}
+path_to_image = {image:path for (image, path) in zip(tile_image_paths, tile_images)}
+image_to_path = {path:image for image, path in zip(tile_image_paths, tile_images)}
 
-SIDE_TILE_LENGTH = SCREEN_HEIGHT // len(TILE_IMAGE_PATHS)
-TILE_WIDTH = MAIN_SCREEN_WIDTH // TILES_PER_ROW
-TILE_HEIGHT = SCREEN_HEIGHT // TILES_PER_COLUMN
+side_tile_length = SCREEN_HEIGHT // len(tile_image_paths)
+tile_width = MAIN_SCREEN_WIDTH // TILES_PER_ROW
+tile_height = SCREEN_HEIGHT // TILES_PER_COLUMN
 
-screen = pg.display.set_mode((MAIN_SCREEN_WIDTH + SIDE_TILE_LENGTH, SCREEN_HEIGHT))
-picked_tile = path_to_image[TILE_IMAGE_PATHS[0]]
+screen = pg.display.set_mode((MAIN_SCREEN_WIDTH + side_tile_length, SCREEN_HEIGHT))
+picked_tile = path_to_image[tile_image_paths[0]]
+
+colors = pg.color.THECOLORS
 
 
 def pick_image():
     if mouseX < MAIN_SCREEN_WIDTH: return
 
-    for ind, image in enumerate(TILE_IMAGES):
-        if mouseY <= (ind+1) * SIDE_TILE_LENGTH:
+    for ind, image in enumerate(tile_images):
+        if mouseY <= (ind+1) * side_tile_length:
             return image
 
 
@@ -38,6 +40,7 @@ def save_map():
         try:
             with open(f"map{save_num}.pkl", "xb") as file:
                 pickle.dump(grid, file)
+
         except FileExistsError:
             save_num += 1
             continue
@@ -47,7 +50,7 @@ def save_map():
 # create grid of Nones to hold tile data
 grid = [[None] * TILES_PER_COLUMN] * TILES_PER_ROW
 
-screen.fill((255, 255, 255))
+screen.fill(colors["white"])
 
 while True:
 
@@ -55,27 +58,27 @@ while True:
     mouseX, mouseY = pg.mouse.get_pos()
     lmb, rmb, mmb = pg.mouse.get_pressed(3)
 
-    pg.draw.line(screen, (0, 0, 0), (800, 0), (800, 800))
+    pg.draw.line(screen, colors["black"], (800, 0), (800, 800))
 
-    for x in range(0, MAIN_SCREEN_WIDTH, TILE_WIDTH):
-        pg.draw.line(screen, pg.colordict.THECOLORS["black"], (x, 0), (x, SCREEN_HEIGHT))
+    for x in range(0, MAIN_SCREEN_WIDTH, tile_width):
+        pg.draw.line(screen, colors["black"], (x, 0), (x, SCREEN_HEIGHT))
 
-    for y in range(0, SCREEN_HEIGHT, TILE_HEIGHT):
-        pg.draw.line(screen, pg.colordict.THECOLORS["black"], (0, y), (MAIN_SCREEN_WIDTH, y))
+    for y in range(0, SCREEN_HEIGHT, tile_height):
+        pg.draw.line(screen, colors["black"], (0, y), (MAIN_SCREEN_WIDTH, y))
 
-    for index, image in enumerate(TILE_IMAGES):
-        scale = (SIDE_TILE_LENGTH,) * 2  # make tuple of number
+    for index, image in enumerate(tile_images):
+        scale = (side_tile_length,) * 2  # make tuple of number
         scaled_image = pg.transform.scale(image, scale)
-        screen.blit(scaled_image, (MAIN_SCREEN_WIDTH, index * SIDE_TILE_LENGTH))
+        screen.blit(scaled_image, (MAIN_SCREEN_WIDTH, index * side_tile_length))
 
     if lmb:
         picked = pick_image()
         if picked:
             picked_tile = picked
         else:
-            grid[mouseX // SIDE_TILE_LENGTH][mouseY // SIDE_TILE_LENGTH] = image_to_path[picked_tile]
-            screen.blit(pg.transform.scale(picked_tile, (TILE_WIDTH, TILE_HEIGHT)),
-                        ((mouseX // TILE_WIDTH) * TILE_HEIGHT, (mouseY // TILE_HEIGHT) * TILE_HEIGHT))
+            grid[mouseX // side_tile_length][mouseY // side_tile_length] = image_to_path[picked_tile]
+            screen.blit(pg.transform.scale(picked_tile, (tile_width, tile_height)),
+                        ((mouseX // tile_width) * tile_height, (mouseY // tile_height) * tile_height))
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
