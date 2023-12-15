94d803c34210816db285d65b15b2b3af8fd5fc7d
Oshawhat <alannobledall@gmail.com>
2023-12-15 09:37:29 -0600

    add dicts to convert from images to image paths

--- LevelCreator.py
+++ LevelCreator.py
@@ -7,24 +7,27 @@ pg.init()
 MAIN_SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 SIDE_MENU_WIDTH = 100
-TILES_PER_ROW = 16
-TILES_PER_COLUMN = 16
+TILES_PER_ROW = 15
+TILES_PER_COLUMN = 15
 
 TILE_IMAGE_PATHS = [f"imgz/{image}" for image in os.listdir("imgz") if image.startswith("tile_")]
+TILE_IMAGES = [pg.image.load(image) for image in TILE_IMAGE_PATHS]
+
+path_to_image = {image:path for (image, path) in zip(TILE_IMAGE_PATHS, TILE_IMAGES)}
+image_to_path = {path:image for image, path in zip(TILE_IMAGE_PATHS, TILE_IMAGES)}
+
 SIDE_TILE_LENGTH = SCREEN_HEIGHT // len(TILE_IMAGE_PATHS)
 TILE_WIDTH = MAIN_SCREEN_WIDTH // TILES_PER_ROW
 TILE_HEIGHT = SCREEN_HEIGHT // TILES_PER_COLUMN
 
 screen = pg.display.set_mode((MAIN_SCREEN_WIDTH + SIDE_TILE_LENGTH, SCREEN_HEIGHT))
-picked_tile = pg.image.load(TILE_IMAGE_PATHS[0])
-
-tile_images = [pg.image.load(image) for image in TILE_IMAGE_PATHS]
+picked_tile = path_to_image[TILE_IMAGE_PATHS[0]]
 
 
 def pick_image():
     if mouseX < MAIN_SCREEN_WIDTH: return
 
-    for ind, image in enumerate(tile_images):
+    for ind, image in enumerate(TILE_IMAGES):
         if mouseY <= (ind+1) * SIDE_TILE_LENGTH:
             return image
 
@@ -33,7 +36,7 @@ def save_map():
     save_num = 1
     while True:
         try:
-            with open(f"map{save_num}", "xb") as file:
+            with open(f"map{save_num}.pkl", "xb") as file:
                 pickle.dump(grid, file)
         except FileExistsError:
             save_num += 1
@@ -60,7 +63,7 @@ while True:
     for y in range(0, SCREEN_HEIGHT, TILE_HEIGHT):
         pg.draw.line(screen, pg.colordict.THECOLORS["black"], (0, y), (MAIN_SCREEN_WIDTH, y))
 
-    for index, image in enumerate(tile_images):
+    for index, image in enumerate(TILE_IMAGES):
         scale = (SIDE_TILE_LENGTH,) * 2  # make tuple of number
         scaled_image = pg.transform.scale(image, scale)
         screen.blit(scaled_image, (MAIN_SCREEN_WIDTH, index * SIDE_TILE_LENGTH))
@@ -70,15 +73,15 @@ while True:
         if picked:
             picked_tile = picked
         else:
-            grid[mouseX // SIDE_TILE_LENGTH][mouseY // SIDE_TILE_LENGTH] = picked_tile
+            grid[mouseX // SIDE_TILE_LENGTH][mouseY // SIDE_TILE_LENGTH] = image_to_path[picked_tile]
             screen.blit(pg.transform.scale(picked_tile, (TILE_WIDTH, TILE_HEIGHT)),
                         ((mouseX // TILE_WIDTH) * TILE_HEIGHT, (mouseY // TILE_HEIGHT) * TILE_HEIGHT))
-
-    if keys[pg.K_SPACE]:
-        save_map()
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                save_map()
 
     pg.display.flip()
