48e3af281295b67b9c6809f493d81561483517f5
Oshawhat <alannobledall@gmail.com>
2024-01-11 10:35:25 -0600

    remove | because school won't update their python ver

--- client_network.py
+++ client_network.py
@@ -49,7 +49,7 @@ class Game_Object(pg.sprite.Sprite):
 
 
 class Player(Game_Object):
-    def __init__(self, player_dict: dict, group: pg.sprite.Group | pg.sprite.GroupSingle, game_images,
+    def __init__(self, player_dict: dict, group, game_images,
                  main_player=False):
         super().__init__(player_dict, group, game_images)
         self.username = player_dict["username"]
